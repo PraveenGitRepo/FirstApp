8279b96c45fed669b0b19717358118e4e0c5b1aa
Praveen <gm.praveen@gmail.com>
2015-04-17 19:05:26 +0530

    user
    userrr

---
+++ JingApp/UserInfo.h
@@ -0,0 +1,13 @@
+//
+//  UserInfo.h
+//  JingApp
+//
+//  Created by Shilpa on 4/17/15.
+//  Copyright (c) 2015 Praveen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UserInfo : NSObject
+
+@end
---
+++ JingApp/UserInfo.m
@@ -0,0 +1,13 @@
+//
+//  UserInfo.m
+//  JingApp
+//
+//  Created by Shilpa on 4/17/15.
+//  Copyright (c) 2015 Praveen. All rights reserved.
+//
+
+#import "UserInfo.h"
+
+@implementation UserInfo
+
+@end
