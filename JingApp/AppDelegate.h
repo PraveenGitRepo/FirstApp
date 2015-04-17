8aa99396fa9b7ec0b376f2b1ca2dc127717e7977
Praveen <gm.praveen@gmail.com>
2015-04-17 18:51:34 +0530

    Initial Commit

---
+++ JingApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JingApp
+//
+//  Created by Shilpa on 4/17/15.
+//  Copyright (c) 2015 Praveen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JingApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JingApp
+//
+//  Created by Shilpa on 4/17/15.
+//  Copyright (c) 2015 Praveen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
